3fdd1a2870d161e91a931442fefbae0d408ac1f3
Atena Mafi <atenamafi20@gmail.com>
2025-02-02 18:16:04 +0330

    add js

--- Home.js
+++ Home.js
@@ -1,7 +1,50 @@
+const searchIcon=document.querySelector("#searchIcon");
+const navbarNav=document.querySelector(".navbar-nav");
+const searchForm=document.querySelector("#searchForm");
+const searchingInput=document.querySelector("#searching");
+const searchBtn=document.querySelector(".search-btn");
+const closeSearchBtn=document.querySelector("#closeSearch");
+searchIcon.addEventListener("click",(e)=>{
+  e.preventDefault();
+  navbarNav.classList.add("d-none");
+  searchForm.classList.remove("d-none");
+  searchingInput.focus();
+
+})
+
+searchBtn.addEventListener("click",(e)=>{
+
+  e.preventDefault();
+  const search=searchingInput.value.trim();
+  if(search){
+  window.location.href=`search.html?query=${encodeURIComponent(search)}`
+
+  }
+});
+
+
+closeSearchBtn.addEventListener("click",(e)=>{
+
+  e.preventDefault();
+  searchForm.classList.add("d-none");
+  navbarNav.classList.remove("d-none");
+
+})
+
+
+searchingInput.addEventListener("keypress",(e)=>{
+
+if(e.ket==="Enter"){
+  searchBtn.click();
+
+}
+
+})
+
+
+
 const indicators=document.querySelector(".carousel-indicators");
 const carouselInner=document.querySelector(".carousel-inner");
-
-
 async function CarouselSlider() {
 try{
    const response= await fetch(`https://corsproxy.io/?${encodeURIComponent("https://api.deezer.com/chart?limit=5")}`);
