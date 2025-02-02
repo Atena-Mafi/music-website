6c9f1ebb780d6a7d6d47c29bb6251756b8760662
Atena Mafi <atenamafi20@gmail.com>
2025-02-02 20:28:04 +0330

    add cods

---
+++ pages/artist.js
@@ -0,0 +1,43 @@
+const searchIcon=document.querySelector("#searchIcon");
+const navbarNav=document.querySelector(".navbar-nav");
+const searchForm=document.querySelector("#searchForm");
+const searchingInput=document.querySelector("#searching");
+const searchBtn=document.querySelector(".search-btn");
+const closeSearchBtn=document.querySelector("#closeSearch");
+searchIcon.addEventListener("click",(e)=>{
+  e.preventDefault();
+  navbarNav.classList.add("d-none");
+  searchForm.classList.remove("d-none");
+  searchingInput.focus();
+
+})
+
+searchBtn.addEventListener("click",(e)=>{
+  searchingInput.value="";
+  e.preventDefault();
+  const search=searchingInput.value.trim();
+  if(search){
+  window.location.href=`search.html?query=${encodeURIComponent(search)}`
+
+  }
+  
+});
+
+
+closeSearchBtn.addEventListener("click",(e)=>{
+
+  e.preventDefault();
+  searchForm.classList.add("d-none");
+  navbarNav.classList.remove("d-none");
+
+})
+
+
+searchingInput.addEventListener("keypress",(e)=>{
+
+if(e.ket==="Enter"){
+  searchBtn.click();
+
+}
+
+})
---
+++ pages/search.js
@@ -0,0 +1,65 @@
+const searchIcon=document.querySelector("#searchIcon");
+const navbarNav=document.querySelector(".navbar-nav");
+const searchForm=document.querySelector("#searchForm");
+const searchingInput=document.querySelector("#searching");
+const searchBtn=document.querySelector(".search-btn");
+const closeSearchBtn=document.querySelector("#closeSearch");
+searchIcon.addEventListener("click",(e)=>{
+  e.preventDefault();
+  navbarNav.classList.add("d-none");
+  searchForm.classList.remove("d-none");
+  searchingInput.focus();
+
+})
+
+searchBtn.addEventListener("click",(e)=>{
+  searchingInput.value="";
+  e.preventDefault();
+  const search=searchingInput.value.trim();
+  if(search){
+  window.location.href=`search.html?query=${encodeURIComponent(search)}`
+
+  }
+  
+});
+
+
+closeSearchBtn.addEventListener("click",(e)=>{
+
+  e.preventDefault();
+  searchForm.classList.add("d-none");
+  navbarNav.classList.remove("d-none");
+
+})
+
+
+searchingInput.addEventListener("keypress",(e)=>{
+
+if(e.ket==="Enter"){
+  searchBtn.click();
+
+}
+
+})
+
+
+const urlParams=new URLSearchParams(window.location.search);
+const searchQuery=urlParams.get("query");
+console.log(searchQuery);
+
+
+
+
+
+
+
+
+
+// <div class="card text-bg-dark">
+//   <img src="..." class="card-img" alt="...">
+//   <div class="card-img-overlay">
+//     <h5 class="card-title">Card title</h5>
+//     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//     <p class="card-text"><small>Last updated 3 mins ago</small></p>
+//   </div>
+// </div>
