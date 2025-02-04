58118cf1698c5ae351920c2d6c6fc6d0b2ff8f09
Atena Mafi <atenamafi20@gmail.com>
2025-02-04 18:07:51 +0330

    code

--- pages/tracks.js
+++ pages/tracks.js
@@ -35,9 +35,52 @@ closeSearchBtn.addEventListener("click",(e)=>{
 
 searchingInput.addEventListener("keypress",(e)=>{
 
-if(e.ket==="Enter"){
-  searchBtn.click();
+    if(e.key==="Enter"){
+        searchBtn.click();
 
 }
 
 })
+
+
+
+const urlParams=new URLSearchParams(window.location.search);
+const genre=urlParams.get("genre");
+
+const cardsContainer=document.querySelector(".cards-container");
+async function musicGenre() {
+
+    try{
+const response=await fetch(`https://api.jamendo.com/v3.0/tracks/?client_id=8cb724ba&format=json&tag=${genre}&limit=20`)
+
+if(!response.ok){
+
+    throw new Error(`Error: ${response.status}`);
+}
+
+const data=await response.json();
+
+const results=data.results;
+results.forEach(track => {
+    const card=document.createElement("div");
+    card.classList.add("card", "text-bg-dark");
+    card.innerHTML=` <img src="${track.image}" class="card-img" alt="${track.name}">
+  <div class="card-img-overlay">
+    <h5 class="card-title">${track.name}</h5>
+    <a class="card-text"><small>${track.artist_name}</small></a>
+  </div>`
+
+  cardsContainer.append(card);
+});
+
+
+
+    }
+    catch(err){
+
+        console.log(err.message);
+        
+    }
+}
+
+musicGenre();
